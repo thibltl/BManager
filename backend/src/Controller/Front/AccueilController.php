316b3fc62782c5396ca2ccd14b9dea6b64c91ad3
KaydapLaFrappe <victor.moraud2004@gmail.com>
2025-12-04 12:10:38 +0100

    Design

--- backend/src/Controller/Front/AccueilController.php
+++ backend/src/Controller/Front/AccueilController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Front;
 
 use App\Repository\ProjectRepository;
+use App\Repository\TasksRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,12 +11,14 @@ use Symfony\Component\Routing\Attribute\Route;
 final class AccueilController extends AbstractController
 {
     #[Route('/accueil', name: 'app_accueil')]
-    public function index(ProjectRepository $projectRepository): Response
+    public function index(ProjectRepository $projectRepository, TasksRepository $tasksRepository): Response
     {
         $projects = $projectRepository->findAll();
+        $tasks = $tasksRepository->findAll();
 
         return $this->render('front/accueil/index.html.twig', [
             'projects' => $projects,
+            'tasks' => $tasks,
         ]);
     }
 }
